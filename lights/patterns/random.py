df795dd6c8ba3c14b93aae0f5bc95415f6a4db74
Chris Johnston <chris@thejohnstons.net>
2018-12-20 01:06:41 -0800

    make random use the delay

--- lights/patterns/random.py
+++ lights/patterns/random.py
@@ -1,14 +1,24 @@
 """
-Scroll pattern
+Random pattern
 """
 
 from .pattern import Pattern
 import random
+import time
 
-class Scroll(Pattern):
+class Random(Pattern):
+    last_time = 0
 
     def __init__(self):
         super(Pattern, self).__init__()
+
+    @staticmethod
+    def __get_time():
+        return time.time() * 1000
+
+    @classmethod
+    def __set_time(self):
+        self.last_time = Random.__get_time()
 
     @classmethod
     def get_id(self):
@@ -16,10 +26,12 @@ class Scroll(Pattern):
 
     @classmethod
     def update(self, strip, state):
-        # set the background to color2
-        strip.fill(state.color2)
+        if Random.__get_time() > (state.delay + self.last_time):
+            self.__set_time()
+            # set the background to color2
+            strip.fill(state.color2)
 
-        for x in range(state.length):
-            # pick a random index to set to color1
-            index = random.randint(0, len(strip) - 1)
-            strip[index] = state.color1
+            for x in range(state.length):
+                # pick a random index to set to color1
+                index = random.randint(0, len(strip) - 1)
+                strip[index] = state.color1
