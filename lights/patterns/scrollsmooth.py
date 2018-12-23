f65a9eb774d5a7b9411c2e2d20e838cdb317a428
Chris Johnston <chjohnston@protonmail.com>
2018-12-22 23:12:51 -0800

    Actually use the fast approx method for the smooth scroll

--- lights/patterns/scrollsmooth.py
+++ lights/patterns/scrollsmooth.py
@@ -26,8 +26,8 @@ class ScrollSmooth(Pattern):
     def update(self, strip, state):
         # logging.info("Smooth")
         for x in range(len(strip)):
-            c1 = [c * (x / float(state.length + 1) + float(time.time()) * 1000.0 / float(state.delay)) for c in state.color1]
+            c1 = [c * fastApprox(x / float(state.length + 1) + float(time.time()) * 1000.0 / float(state.delay)) for c in state.color1]
             # c2 is out of phase by 1
-            c2 = [c * (x / float(state.length + 1) + 1 + float(time.time()) * 1000.0 / float(state.delay)) for c in state.color2]
+            c2 = [c * fastApprox(x / float(state.length + 1) + 1 + float(time.time()) * 1000.0 / float(state.delay)) for c in state.color2]
             c3 = tuple(constrain_int(a + b) for a, b in zip(c1, c2))
             strip[x] = c3
